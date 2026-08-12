54108daa722f047d5a6e2e91d221162b06a60d94
Whitney <whitgunt77@gmail.com>
2026-08-12 09:34:57 -0400

    Complete Part 2 Exercises 2.1-2.5

---
+++ part1/courseinfo/src/components/Course.jsx
@@ -0,0 +1,38 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  );
+};
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+export default Course;
