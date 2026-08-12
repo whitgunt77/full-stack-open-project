c3b81a0cd990d6bdd016524e7aac535d1e6357a0
Whitney <whitgunt77@gmail.com>
2026-08-12 10:13:09 -0400

    Complete exercises 2.6-2.10

---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,13 @@
+const Persons = ({ persons }) => {
+    return (
+        <>
+          {persons.map(person => (
+            <p key={person.id}>
+                {person.name} {person.number}
+            </p>
+          ))}
+        </>
+    )
+}
+
+export default Persons;
